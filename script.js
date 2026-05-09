2306c8ab5f66b37745e01f20fa20142111d38bb9
tanlianjun1998-netizen <tanlianjun1998@gmail.com>
2026-05-09 11:05:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,13 @@ function submitOrder() {
   const username = document.getElementById("username").value;
   const contact = document.getElementById("contact").value;
 
-  if (!username || !contact) {
-    alert("Please fill all fields!");
+  if(!username || !contact){
+    alert("Please fill all fields");
     return;
   }
 
-  // Save locally (demo)
-  console.log({
-    product,
-    username,
-    contact
-  });
-
   document.getElementById("msg").innerText =
-    "Order submitted successfully! We will contact you soon.";
+  "Order submitted successfully! We will contact you soon.";
 
   document.getElementById("username").value = "";
   document.getElementById("contact").value = "";
